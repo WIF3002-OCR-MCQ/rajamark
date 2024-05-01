77d539a2055e0ed1131387bca285dce2a56d36a2
ikhwanmohd2002 <ikhwanmohd2002@gmail.com>
2024-05-01 21:19:16 +0800

    added statistics widget for desktop in exam detail page

--- rajamarkapp/lib/dashboard/exam_detail.dart
+++ rajamarkapp/lib/dashboard/exam_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class ExamDetailsView extends StatelessWidget {
   final int examId;
@@ -68,10 +69,10 @@ class ExamDetailsView extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             Container(
-              height: 200.0,
-              color: Colors.grey[200],
+              height: 400,
               child: Center(
-                child: Text('Place for a widget'),
+                child:
+                    StatisticsWidget(), //Mean Score, Median Score with vertical divider
               ),
             ),
             SizedBox(height: 16.0),
@@ -167,6 +168,189 @@ class ExamDetailsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class StatisticsWidget extends StatelessWidget {
+  const StatisticsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        MeanDonutChart(),
+        SizedBox(
+            height: 312,
+            child: VerticalDivider(
+              thickness: 2,
+              width: 20,
+              color: Color(0xff88899D),
+            )),
+        MedianDonutChart(),
+      ],
+    );
+  }
+}
+
+class MeanDonutChart extends StatefulWidget {
+  const MeanDonutChart({super.key});
+
+  @override
+  State<MeanDonutChart> createState() => _MeanDonutChartState();
+}
+
+class _MeanDonutChartState extends State<MeanDonutChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          width: 362,
+          height: 312,
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  'Mean Score',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                    height: 247,
+                    width: 247,
+                    child: Stack(
+                      children: [
+                        PieChart(PieChartData(
+                            startDegreeOffset: 90,
+                            sectionsSpace: 0,
+                            sections: [
+                              PieChartSectionData(
+                                  value: 81,
+                                  color: Color(0xff6FB293),
+                                  radius: 40,
+                                  showTitle: false),
+                              PieChartSectionData(
+                                  value: 19,
+                                  color: Color(0xffF0E5FC),
+                                  radius: 40,
+                                  showTitle: false)
+                            ])),
+                        Positioned.fill(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 180,
+                                width: 150,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    '81%',
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MedianDonutChart extends StatefulWidget {
+  const MedianDonutChart({super.key});
+
+  @override
+  State<MedianDonutChart> createState() => _MedianDonutChartState();
+}
+
+class _MedianDonutChartState extends State<MedianDonutChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          width: 362,
+          height: 312,
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  'Median Score',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                    height: 247,
+                    width: 247,
+                    child: Stack(
+                      children: [
+                        PieChart(PieChartData(
+                            startDegreeOffset: 90,
+                            sectionsSpace: 0,
+                            sections: [
+                              PieChartSectionData(
+                                  value: 91,
+                                  color: Color(0xff6FB293),
+                                  radius: 40,
+                                  showTitle: false),
+                              PieChartSectionData(
+                                  value: 9,
+                                  color: Color(0xffF0E5FC),
+                                  radius: 40,
+                                  showTitle: false)
+                            ])),
+                        Positioned.fill(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 180,
+                                width: 150,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    '90%',
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
