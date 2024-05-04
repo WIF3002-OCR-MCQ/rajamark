d0db94b9d3cb3ca5028f6ebbe27265f1756b6f80
garrren <garrenlutherp@gmail.com>
2024-05-04 19:22:23 +0800

    chore: updated colours in constants

--- rajamarkapp/lib/const/constant.dart
+++ rajamarkapp/lib/const/constant.dart
@@ -7,3 +7,4 @@ const blueButton = Color(0xFFBFD7ED);
 const blueButtonText = Color(0xFF0074B7);
 const dividerColour = Color(0xFF88899D);
 const correctColour = Color(0xff6fb293);
+const popupBackground = Color(0xFFBFD7E7);
