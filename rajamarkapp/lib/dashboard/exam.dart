3f49f643260d59a723fc3363108433ec3bc9bbe2
garrren <garrenlutherp@gmail.com>
2024-05-04 19:13:18 +0800

    refactor: remove unused import in exam.dart

--- rajamarkapp/lib/dashboard/exam.dart
+++ rajamarkapp/lib/dashboard/exam.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:rajamarkapp/const/constant.dart';
 import 'package:rajamarkapp/dashboard/exam_detail.dart';
 
 import '../popups/delete_popup.dart';
