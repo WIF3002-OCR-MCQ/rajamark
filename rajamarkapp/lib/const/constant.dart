f3ce73c1761865e75fa7da4352d1efab8d5cbabd
garrren <garrenlutherp@gmail.com>
2024-05-04 18:13:30 +0800

    refactor: added more colour to constants file

--- rajamarkapp/lib/const/constant.dart
+++ rajamarkapp/lib/const/constant.dart
@@ -6,3 +6,4 @@ const warningColour = Color(0xFFE54A50);
 const blueButton = Color(0xFFBFD7ED);
 const blueButtonText = Color(0xFF0074B7);
 const dividerColour = Color(0xFF88899D);
+const correctColour = Color(0xff6fb293);
