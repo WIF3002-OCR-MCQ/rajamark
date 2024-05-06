d504ec15f492e1d3485187d54bbe50b38af33bba
ahmadileham <96009848+ahmadileham@users.noreply.github.com>
2024-05-07 00:25:14 +0800

    backend code for auth module

---
+++ rajamarkapp/lib/services/auth_service.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print('Error signing in: $e');
+      return null;
+    }
+  }
+
+  Future<User?> registerWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print('Error registering user: $e');
+      return null;
+    }
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      print('Error sending password reset email: $e');
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
