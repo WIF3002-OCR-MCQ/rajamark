90728ef62ad5088980e19bbfbb0b6f014b1a3421
Tan Zi Yang <jacktzy7899@gmail.com>
2024-05-13 14:34:31 +0800

    refactor firebase secrets

--- rajamarkapp/lib/database/database.dart
+++ rajamarkapp/lib/database/database.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:io';
 
 class FirebaseInitialization {
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env");
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyAkhF8Nmtr2ou0mwenA7WzCRLyAbyQo13g",
-        authDomain: "ocrsystemformcqanswersheet.firebaseapp.com",
-        projectId: "ocrsystemformcqanswersheet",
-        storageBucket: "ocrsystemformcqanswersheet.appspot.com",
-        messagingSenderId: "468830889753",
-        appId: "1:468830889753:web:99d84c4a6abc2ea1174788",
-        measurementId: "G-7XGK9X56VL",
-      ),
-    );
+        options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      authDomain: dotenv.env['AUTH_DOMAIN']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+      storageBucket: dotenv.env['STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['APP_ID']!,
+      measurementId: dotenv.env['MEASUREMENT_ID']!,
+    ));
   }
 }
 
@@ -78,18 +79,19 @@ Future<void> createExam(Map<String, dynamic> examData) async {
 
   try {
     DocumentReference doc = await exams.add(examData);
-    print('Exam created successfully! '+ doc.id);
+    print('Exam created successfully! ' + doc.id);
   } catch (e) {
     print('Error creating exam: $e');
   }
 }
 
-Future<void> updateExamById(String examId, Map<String, dynamic> updatedExamData) async {
+Future<void> updateExamById(
+    String examId, Map<String, dynamic> updatedExamData) async {
   CollectionReference exams = FirebaseFirestore.instance.collection('exam');
 
   try {
     await exams.doc(examId).update(updatedExamData);
-    print('Exam updated successfully! '+ examId);
+    print('Exam updated successfully! ' + examId);
   } catch (e) {
     print('Error updating exam: $e');
   }
@@ -111,7 +113,9 @@ Future<List<Map<String, dynamic>>> getAllExams() async {
 
   try {
     QuerySnapshot querySnapshot = await exams.get();
-    List<Map<String, dynamic>> examList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    List<Map<String, dynamic>> examList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
     return examList;
   } catch (e) {
     print('Error retrieving exams: $e');
@@ -136,7 +140,6 @@ Future<Map<String, dynamic>?> getExamById(String examId) async {
   }
 }
 
-
 //-------------- Student_Result-------------
 // Create Student
 // String? downloadURL = await saveStudentResultImage();
@@ -145,74 +148,90 @@ Future<Map<String, dynamic>?> getExamById(String examId) async {
 
 Future<String?> saveStudentResultImage(File imageFile) async {
   try {
-    String fileName = 'result_${DateTime.now().millisecondsSinceEpoch}.jpg'; // Generate a unique file name for the image
-    Reference storageReference = FirebaseStorage.instance.ref().child('student-result-images/$fileName');
+    String fileName =
+        'result_${DateTime.now().millisecondsSinceEpoch}.jpg'; // Generate a unique file name for the image
+    Reference storageReference =
+        FirebaseStorage.instance.ref().child('student-result-images/$fileName');
     UploadTask uploadTask = storageReference.putFile(imageFile);
     TaskSnapshot snapshot = await uploadTask.whenComplete(() => null);
     String downloadURL = await snapshot.ref.getDownloadURL();
     return downloadURL;
-
   } catch (e) {
     print('Error saving student result image: $e');
     return null;
   }
 }
 
-
 //pass in the student result map
 Future<void> createStudentResult(Map<String, dynamic> studentData) async {
-  CollectionReference student_result = FirebaseFirestore.instance.collection('student_result');
+  CollectionReference student_result =
+      FirebaseFirestore.instance.collection('student_result');
 
   try {
     DocumentReference doc = await student_result.add(studentData);
-    print('Student Result created successfully! '+ doc.id);
+    print('Student Result created successfully! ' + doc.id);
   } catch (e) {
     print('Error creating exam: $e');
   }
 }
 
-Future<List<Map<String, dynamic>>> getStudentResultByStudentId(String studentId) async {
+Future<List<Map<String, dynamic>>> getStudentResultByStudentId(
+    String studentId) async {
   try {
-    CollectionReference resultsCollection = FirebaseFirestore.instance.collection('student_result');
-    QuerySnapshot querySnapshot = await resultsCollection.where('student_id', isEqualTo: studentId).get();
-    List<Map<String, dynamic>> resultsList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    CollectionReference resultsCollection =
+        FirebaseFirestore.instance.collection('student_result');
+    QuerySnapshot querySnapshot =
+        await resultsCollection.where('student_id', isEqualTo: studentId).get();
+    List<Map<String, dynamic>> resultsList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
     return resultsList;
-
-  } catch (e) {
-    print('Error getting student results: $e');
-    return [];
-  }
-
-}
-
-Future<List<Map<String, dynamic>>> getStudentResultByExamId(String examId) async {
-  try {
-    CollectionReference resultsCollection = FirebaseFirestore.instance.collection('student_result');
-    QuerySnapshot querySnapshot = await resultsCollection.where('exam_id', isEqualTo: examId).get();
-    List<Map<String, dynamic>> resultsList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-    return resultsList;
-
   } catch (e) {
     print('Error getting student results: $e');
     return [];
   }
 }
 
-Future<List<Map<String, dynamic>>> getStudentResultByStudentIdAndExamId(String studentId, String examId) async {
+Future<List<Map<String, dynamic>>> getStudentResultByExamId(
+    String examId) async {
   try {
-    CollectionReference resultsCollection = FirebaseFirestore.instance.collection('student_result');
-    QuerySnapshot querySnapshot = await resultsCollection.where('exam_id', isEqualTo: examId).where('student_id', isEqualTo: studentId).get();
-    List<Map<String, dynamic>> resultsList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    CollectionReference resultsCollection =
+        FirebaseFirestore.instance.collection('student_result');
+    QuerySnapshot querySnapshot =
+        await resultsCollection.where('exam_id', isEqualTo: examId).get();
+    List<Map<String, dynamic>> resultsList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
     return resultsList;
-
   } catch (e) {
     print('Error getting student results: $e');
     return [];
   }
 }
 
-Future<void> updateStudentResultByExamIdAndStudentId(String examId, String studentId, Map<String, dynamic> updatedStudentResult) async {
-  CollectionReference studentResults = FirebaseFirestore.instance.collection('student_result');
+Future<List<Map<String, dynamic>>> getStudentResultByStudentIdAndExamId(
+    String studentId, String examId) async {
+  try {
+    CollectionReference resultsCollection =
+        FirebaseFirestore.instance.collection('student_result');
+    QuerySnapshot querySnapshot = await resultsCollection
+        .where('exam_id', isEqualTo: examId)
+        .where('student_id', isEqualTo: studentId)
+        .get();
+    List<Map<String, dynamic>> resultsList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+    return resultsList;
+  } catch (e) {
+    print('Error getting student results: $e');
+    return [];
+  }
+}
+
+Future<void> updateStudentResultByExamIdAndStudentId(String examId,
+    String studentId, Map<String, dynamic> updatedStudentResult) async {
+  CollectionReference studentResults =
+      FirebaseFirestore.instance.collection('student_result');
   try {
     // Query to retrieve the specific document where both studentId and examId match
     QuerySnapshot querySnapshot = await studentResults
@@ -229,17 +248,21 @@ Future<void> updateStudentResultByExamIdAndStudentId(String examId, String stude
       // Update the document with the new data
       await docRef.update(updatedStudentResult);
 
-      print('Student result for student ID $studentId and exam ID $examId updated successfully!');
+      print(
+          'Student result for student ID $studentId and exam ID $examId updated successfully!');
     } else {
-      print('No student result found for student ID $studentId and exam ID $examId.');
+      print(
+          'No student result found for student ID $studentId and exam ID $examId.');
     }
   } catch (e) {
     print('Error updating student result: $e');
   }
 }
 
-Future<void> deleteStudentResultByExamIdAndStudentId(String examId, String studentId) async {
-  CollectionReference studentResults = FirebaseFirestore.instance.collection('student_result');
+Future<void> deleteStudentResultByExamIdAndStudentId(
+    String examId, String studentId) async {
+  CollectionReference studentResults =
+      FirebaseFirestore.instance.collection('student_result');
   try {
     // Query to retrieve the specific document where both studentId and examId match
     QuerySnapshot querySnapshot = await studentResults
@@ -256,15 +279,16 @@ Future<void> deleteStudentResultByExamIdAndStudentId(String examId, String stude
       // Delete the document
       await docRef.delete();
 
-      print('Student result for student ID $studentId and exam ID $examId deleted successfully!');
+      print(
+          'Student result for student ID $studentId and exam ID $examId deleted successfully!');
     } else {
-      print('No student result found for student ID $studentId and exam ID $examId.');
+      print(
+          'No student result found for student ID $studentId and exam ID $examId.');
     }
   } catch (e) {
     print('Error deleting student result: $e');
   }
 }
-
 
 //----------grade-------------------
 Future<void> createGrade(Map<String, dynamic> gradeData) async {
@@ -272,7 +296,7 @@ Future<void> createGrade(Map<String, dynamic> gradeData) async {
 
   try {
     DocumentReference doc = await grade.add(gradeData);
-    print('Grade created successfully! '+ doc.id);
+    print('Grade created successfully! ' + doc.id);
   } catch (e) {
     print('Error creating grade:$e');
   }
@@ -282,31 +306,39 @@ Future<List<Map<String, dynamic>>> getGradesByExamId(String examId) async {
   CollectionReference grade = FirebaseFirestore.instance.collection('grade');
 
   try {
-    QuerySnapshot querySnapshot = await grade.where('exam_id', isEqualTo: examId).get();
-    List<Map<String, dynamic>> resultsList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    QuerySnapshot querySnapshot =
+        await grade.where('exam_id', isEqualTo: examId).get();
+    List<Map<String, dynamic>> resultsList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
     return resultsList;
-
   } catch (e) {
     print('Error getting grade: $e');
     return [];
   }
 }
 
-Future<List<Map<String, dynamic>>> getGradeByExamIdAndGradeLabel(String examId, String gradeLabel) async {
+Future<List<Map<String, dynamic>>> getGradeByExamIdAndGradeLabel(
+    String examId, String gradeLabel) async {
   CollectionReference grade = FirebaseFirestore.instance.collection('grade');
 
   try {
-    QuerySnapshot querySnapshot = await grade.where('exam_id', isEqualTo: examId).where('grade_label', isEqualTo: gradeLabel).get();
-    List<Map<String, dynamic>> resultsList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    QuerySnapshot querySnapshot = await grade
+        .where('exam_id', isEqualTo: examId)
+        .where('grade_label', isEqualTo: gradeLabel)
+        .get();
+    List<Map<String, dynamic>> resultsList = querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
     return resultsList;
-
   } catch (e) {
     print('Error getting grade: $e');
     return [];
   }
 }
 
-Future<void> updateGradeByExamIdAndGradeLabel(String examId, String gradeLabel, Map<String, dynamic> updatedGradeData) async {
+Future<void> updateGradeByExamIdAndGradeLabel(String examId, String gradeLabel,
+    Map<String, dynamic> updatedGradeData) async {
   CollectionReference grade = FirebaseFirestore.instance.collection('grade');
   try {
     // Query to retrieve the specific document where both studentId and examId match
@@ -324,9 +356,11 @@ Future<void> updateGradeByExamIdAndGradeLabel(String examId, String gradeLabel, 
       // Update the document with the new data
       await docRef.update(updatedGradeData);
 
-      print('Student result for grade label ID $gradeLabel and exam ID $examId updated successfully!');
+      print(
+          'Student result for grade label ID $gradeLabel and exam ID $examId updated successfully!');
     } else {
-      print('No student result found for grade label ID $gradeLabel and exam ID $examId.');
+      print(
+          'No student result found for grade label ID $gradeLabel and exam ID $examId.');
     }
   } catch (e) {
     print('Error updating student result: $e');
